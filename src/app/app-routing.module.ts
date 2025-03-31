bfeb728abd3544340273971c1dec06108a4a6e44
mateus <mateus26guerra@gmail.com>
2025-03-31 15:45:05 -0300

    feat: descrição da mudança

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { IndexComponent } from './components/index/index.component';
+import { MainAtividadeComponent } from './components/main-atividade/main-atividade.component';
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'home', component: MainAtividadeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,26 @@ import { AppComponent } from './app.component';
 import { AdicionarUsuarioComponent } from './components/adicionar-usuario/adicionar-usuario.component';
 import { MainAtividadeComponent } from './components/main-atividade/main-atividade.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { IndexComponent } from './components/index/index.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdicionarUsuarioComponent,
     MainAtividadeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
